1c5d528901908dc063d6b6a37bbe3c0b51c6515f
catalinpit91 <92463435+catalinpit91@users.noreply.github.com>
2022-01-25 09:50:09 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,22 @@
+<script type="text/javascript">
+  var winnerButton = Math.floor(Math.random() * 3);
+  function addButtons() {
+    for (let i = 0; i < 3; ++i) {
+      $('#Buttons').append(`        
+        <a href="#" id = "`+ i +`" class="btn btn-primary" onclick="return checkWinnerButton(` + i + `);">choose a button</a>
+      `);
+    }
+    return false;
+  }
+
+  function checkWinnerButton(id) {   
+    if (id == winnerButton) {
+      alert("You won!");
+    } else {
+      alert("Try again!");
+    }
+  } 
+  function newGame() {
+    location.reload();
+  }
+</script>
